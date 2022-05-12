1dd48eb729b9d99c2eadc9dd4dc505ada7cc14d6
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-05-12 23:09:44 +0300

    context connection string was fixed.

--- BlogApp/Models/BlogContext.cs
+++ BlogApp/Models/BlogContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,18 @@ namespace BlogApp.Models
 {
     public class BlogContext:DbContext
     {
+        private IConfiguration _configuration;
+        public BlogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Superuser> Superusers { get; set; }
         public DbSet<New> News { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Data Source=.\\SQLEXPRESS;database=blogapp; integrated security=SSPI");
+            => options.UseSqlServer(_configuration.GetConnectionString("MsSqlConnection"));
         public BlogContext(DbContextOptions<BlogContext> options)
             : base(options)
         { }
