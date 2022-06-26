c98625d7e840e49bbbb97d9969d77a52822e7dde
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-06-26 04:52:28 +0300

    Connection düzeltildi

--- BlogApp/Models/BlogContext.cs
+++ BlogApp/Models/BlogContext.cs
@@ -9,20 +9,14 @@ namespace BlogApp.Models
 {
     public class BlogContext:DbContext
     {
-        private IConfiguration _configuration;
-        public BlogContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Superuser> Superusers { get; set; }
         public DbSet<New> News { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(_configuration.GetConnectionString("MsSqlConnection"));
-        public BlogContext(DbContextOptions<BlogContext> options)
-            : base(options)
-        { }
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
     }
 }
